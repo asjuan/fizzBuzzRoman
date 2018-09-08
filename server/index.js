e0fb56a55bf2580324dd204acffe2bb353bf71c0
asjuan <juan.alsu@gmail.com>
2018-09-08 15:25:45 -0500

    fizz buzz backend operational

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
+const bodyParser = require("body-parser");
 var app = express();
 var match = require("../server/app/fizzBuzzMatch");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
     res.send('Fizz Buzz Server is up..');
 });
@@ -20,16 +25,14 @@ app.post('/add', function (req, res) {
     res.send(playerInfo);
 });
 app.post("/challenge", function (req, res) {
-    debugger;
     var challenge = match.challengePlayer(req.body);
     console.log("Player " + req.body.id + " challenge is " + challenge);
-    res.send(challenge);
+    res.send(JSON.stringify({ "challenge": challenge }));
 });
 app.post("/answer", function (req, res) {
-    debugger;
     var answer = req.body.answer;
     match.respond(req.body.player, answer);
-    console.log("Answer " + req.body.player.id + " answers " + answer);
-    res.send(challenge);
+    console.log("Answer " + req.body.player.id + " he or she answers " + answer);
+    res.send();
 });
 app.listen(3000);
