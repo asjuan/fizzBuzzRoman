3324d0a1c5da58f12c591a5881a1ca7401a053bc
asjuan <juan.alsu@gmail.com>
2018-09-08 23:02:39 -0500

    bootstrap index page

--- client/src/main.js
+++ client/src/main.js
@@ -2,21 +2,24 @@ const settings = require("../src/conf");
 const mainplay = require("../src/play");
 const respond2Me = require("../src/respond");
 (function () {
+    var myplay, myres, reg = document.getElementById("register");
     function joinMe() {
         var myRequest = new XMLHttpRequest();
-        myRequest.open("POST", settings.webapihost +  "/add");
+        myRequest.open("POST", settings.webapihost + "/add");
         myRequest.overrideMimeType("application/json");
         myRequest.send();
         myRequest.onload = function () {
             var myid = document.getElementById("myuserId");
-            var mytoken= document.getElementById("mytoken");
+            var mytoken = document.getElementById("mytoken");
             var obj = JSON.parse(myRequest.response);
-            myid.textContent  = obj.id;
-            mytoken.textContent = obj.token; 
+            myid.textContent = obj.id;
+            mytoken.textContent = obj.token;
+            debugger;
+            reg.disabled = true;
             console.log(myRequest.response);
         };
     }
-    var myplay, myres, reg = document.getElementById("register");
+
     reg.addEventListener("click", joinMe, false);
     myplay = document.getElementById("play");
     myplay.addEventListener("click", mainplay, false);
